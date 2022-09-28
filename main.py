6019e5f87dece3f3e00bb0db15d5c7f116e28996
Hefin-Gtg <16520178@std.stei.itb.ac.id>
2022-09-28 19:01:41 +0700

    Changes

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-from fastapi import APIRouter
-app = APIRouter()
-@app.get("/")
-async def hello() -> dict:
-    return {
-    "message": "Hello World"
-    }
-student_list = []
+# Hefin Immanuel Ginting
+# 18220065
+
+from http.client import HTTPException
+import json
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class student_item(BaseModel):
+    Nama: str
+    NIM: int
+
+data = []
+
+with open("student.json", "r") as read_file: 
+    data = json.load(read_file)
+
+app = FastAPI()
+@app.get('/student/{NIM}')
+async def read_student(NIM: int):
+    for student in data['student']:
+        if student['NIM'] == NIM:
+            return student
+    raise HTTPException(
+        status_code=404, detail=f'Item not found')
+
 @app.post("/student")
-async def add_student(student: dict) -> dict:
-student_list.append(student)
-return {"message": "Sucess"}
-@app.get("/student")
-async def get_student() -> dict:
-return {"student": student_list}
+async def add_student(student_new: student_item):
+    new_student = [
+        {
+            "NIM": student_new.NIM,
+            "NAMA": student_new.Nama
+        }
+    ]
+    data['student'].append(new_student)
+    return data
